17b78bc4cbc2f90d1b52026c07295da95f9f0160
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-08-12 15:50:12 -0600

    Create renderBoard function and insert symbol to bard

--- main.js
+++ main.js
@@ -4,5 +4,27 @@ const gameManager = (() => {
     [" ", " ", " "],
     [" ", " ", " "],
   ];
-  const documentBoard = document.querySelectorAll(".game-block");
+  const inGameBoard = Array.from(document.querySelectorAll(".game-block"));
+
+  const renderBoard = () => {
+    let counter = 0;
+
+    for (let i = 0; i < gameBoard.length; i++) {
+      for (let j = 0; j < gameBoard[i].length; j++) {
+        inGameBoard[counter++].textContent = gameBoard[i][j];
+      }
+    }
+  };
+
+  const insertBlock = (string, row, column) => {
+    gameBoard[row][column] = string;
+    console.log(gameBoard);
+
+    renderBoard();
+  };
+
+  return { insertBlock };
 })();
+
+gameManager.insertBlock("O", 0, 0);
+gameManager.insertBlock("X", 1, 0);
