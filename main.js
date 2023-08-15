f0268ad226234cd0fbb2df7670d769dff47db73d
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-08-14 18:02:01 -0600

    Change bidimensional array to regular array board

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-const gameBoard = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
+const gameBoard = ["", "", "", "", "", "", "", "", ""];
 const inGameBoard = document.querySelectorAll(".game-block");
 
 const gameManager = (() => {
   const renderBoard = () => {
-    let counter = 0;
-
-    for (let i = 0; i < gameBoard.length; i++) {
-      for (let j = 0; j < gameBoard[i].length; j++) {
-        inGameBoard[counter++].textContent = gameBoard[i][j];
-      }
-    }
+    gameBoard.forEach((cell, index) => {
+      inGameBoard[index].textContent = cell;
+    });
   };
-
-  const checkBoard = () => {};
 
   return { renderBoard };
 })();
@@ -24,11 +14,11 @@ const gameManager = (() => {
 const playerFactory = (symbolString) => {
   const symbol = symbolString;
 
-  const insert = (row, column) => {
+  const insert = (cellIndex) => {
     // Prevent overwriting symbols
-    if (!!gameBoard[row][column]) return;
+    if (Boolean(gameBoard[cellIndex])) return;
 
-    gameBoard[row][column] = symbol;
+    gameBoard[cellIndex] = symbol;
 
     gameManager.renderBoard();
   };
@@ -39,22 +29,4 @@ const playerFactory = (symbolString) => {
 const playerOne = playerFactory("X");
 const playerTwo = playerFactory("O");
 
-// Row win
-// playerOne.insert(1, 0);
-// playerOne.insert(1, 1);
-// playerOne.insert(1, 2);
-
-// Column win
-// playerOne.insert(0, 2);
-// playerOne.insert(1, 2);
-// playerOne.insert(2, 2);
-
-// Diagonal win 1
-// playerOne.insert(0, 0);
-// playerOne.insert(1, 1);
-// playerOne.insert(2, 2);
-
-// Diagonal win 2
-// playerOne.insert(0, 2);
-// playerOne.insert(1, 1);
-// playerOne.insert(2, 0);
+playerOne.insert(0);
