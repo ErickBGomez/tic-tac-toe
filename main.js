21e3b80783f64fb756bda89a15c088b9901c8d19
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-08-25 10:41:27 -0600

    Check minimax working correctly

--- main.js
+++ main.js
@@ -221,62 +221,62 @@ const computerFactory = (difficulty, symbolString, playerName = "Computer") => {
 
 gameManager.startGame();
 
-// const minimax = (newBoard, playerSymbol) => {
-//   const availableCells = gameManager.getEmptyCells();
+const minimax = (newBoard, playerSymbol) => {
+  const availableCells = gameManager.getEmptyCells();
 
-//   if (gameManager.checkWin(players[0].getSymbol())) {
-//     return { score: -10 };
-//   } else if (gameManager.checkWin(players[1].getSymbol())) {
-//     return { score: 10 };
-//   } else if (availableCells.length === 0) {
-//     return { score: 0 };
-//   }
+  if (gameManager.checkWin(players[0].getSymbol())) {
+    return { score: -10 };
+  } else if (gameManager.checkWin(players[1].getSymbol())) {
+    return { score: 10 };
+  } else if (availableCells.length === 0) {
+    return { score: 0 };
+  }
 
-//   const moves = [];
+  const moves = [];
 
-//   for (let i = 0; i < availableCells.length; i++) {
-//     const move = {};
-//     let result;
+  for (let i = 0; i < availableCells.length; i++) {
+    const move = {};
+    let result;
 
-//     // Possible fix: Change empty cells to numbers
-//     move.index = newBoard[availableCells[i]];
-//     newBoard[availableCells[i]] = playerSymbol;
+    // Possible fix: Change empty cells to numbers
+    move.index = newBoard[availableCells[i]];
+    newBoard[availableCells[i]] = playerSymbol;
 
-//     if (playerSymbol == players[1].getSymbol()) {
-//       result = minimax(newBoard, players[0].getSymbol());
-//       move.score = result.score;
-//     } else {
-//       result = minimax(newBoard, players[1].getSymbol());
-//       move.score = result.score;
-//     }
+    if (playerSymbol == players[1].getSymbol()) {
+      result = minimax(newBoard, players[0].getSymbol());
+      move.score = result.score;
+    } else {
+      result = minimax(newBoard, players[1].getSymbol());
+      move.score = result.score;
+    }
 
-//     newBoard[availableCells[i]] = move.index;
+    newBoard[availableCells[i]] = move.index;
 
-//     moves.push(move);
-//   }
+    moves.push(move);
+  }
 
-//   let bestMove;
-//   let bestScore;
+  let bestMove;
+  let bestScore;
 
-//   if (playerSymbol === players[1].getSymbol()) {
-//     bestScore = -Infinity;
+  if (playerSymbol === players[1].getSymbol()) {
+    bestScore = -Infinity;
 
-//     for (let i = 0; i < moves.length; i++) {
-//       if (moves[i].score > bestScore) {
-//         bestScore = moves[i].score;
-//         bestMove = i;
-//       }
-//     }
-//   } else {
-//     bestScore = Infinity;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    bestScore = Infinity;
 
-//     for (let i = 0; i < moves.length; i++) {
-//       if (moves[i].score < bestScore) {
-//         bestScore = moves[i].score;
-//         bestMove = i;
-//       }
-//     }
-//   }
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
 
-//   return moves[bestMove];
-// };
+  return moves[bestMove];
+};
